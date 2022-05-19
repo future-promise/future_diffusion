f28f71602551ad25edf40e7cbc4679cfa6cb7562
Jack English <jackjenglish@gmail.com>
2022-05-19 23:21:32 +0100

    remove main settings

--- disco.py
+++ disco.py
@@ -4,23 +4,18 @@ from future_diffusion.discoSetup import *
 import torch
 from dataclasses import dataclass
 from functools import partial
-import cv2
-import pandas as pd
 import gc
 import io
 import math
 from IPython import display
 import lpips
-from PIL import Image, ImageOps
-import requests
-import json
+from PIL import Image
 from torch import nn
 from torch.nn import functional as F
 import torchvision.transforms as T
 import torchvision.transforms.functional as TF
 from tqdm.notebook import tqdm
 from CLIP import clip
-from resize_right import resize
 from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
@@ -676,75 +671,3 @@ from future_diffusion.DiscoAnimate import *
 """### Extra Settings
  Partial Saves, Advanced Settings, Cutn Scheduling
 """
-
-#@markdown ####**Saving:**
-
-intermediate_saves = 0#@param{type: 'raw'}
-intermediates_in_subfolder = True #@param{type: 'boolean'}
-#@markdown Intermediate steps will save a copy at your specified intervals. You can either format it as a single integer or a list of specific steps 
-
-#@markdown A value of `2` will save a copy at 33% and 66%. 0 will save none.
-
-#@markdown A value of `[5, 9, 34, 45]` will save at steps 5, 9, 34, and 45. (Make sure to include the brackets)
-
-
-if type(intermediate_saves) is not list:
-    if intermediate_saves:
-        steps_per_checkpoint = math.floor((steps - skip_steps - 1) // (intermediate_saves+1))
-        steps_per_checkpoint = steps_per_checkpoint if steps_per_checkpoint > 0 else 1
-        print(f'Will save every {steps_per_checkpoint} steps')
-    else:
-        steps_per_checkpoint = steps+10
-else:
-    steps_per_checkpoint = None
-
-if intermediate_saves and intermediates_in_subfolder is True:
-    partialFolder = f'{batchFolder}/partials'
-    createPath(partialFolder)
-
-#@markdown ---
-
-#@markdown ####**Advanced Settings:**
-#@markdown *There are a few extra advanced settings available if you double click this cell.*
-
-#@markdown *Perlin init will replace your init, so uncheck if using one.*
-
-perlin_init = False  #@param{type: 'boolean'}
-perlin_mode = 'mixed' #@param ['mixed', 'color', 'gray']
-set_seed = 'random_seed' #@param{type: 'string'}
-eta = 0.8#@param{type: 'number'}
-clamp_grad = True #@param{type: 'boolean'}
-clamp_max = 0.05 #@param{type: 'number'}
-
-
-### EXTRA ADVANCED SETTINGS:
-randomize_class = True
-clip_denoised = False
-fuzzy_prompt = False
-rand_mag = 0.05
-
-
-#@markdown ---
-
-#@markdown ####**Cutn Scheduling:**
-#@markdown Format: `[40]*400+[20]*600` = 40 cuts for the first 400 /1000 steps, then 20 for the last 600/1000
-
-#@markdown cut_overview and cut_innercut are cumulative for total cutn on any given step. Overview cuts see the entire image and are good for early structure, innercuts are your standard cutn.
-
-cut_overview = "[12]*400+[4]*600" #@param {type: 'string'}       
-cut_innercut ="[4]*400+[12]*600"#@param {type: 'string'}  
-cut_ic_pow = 1#@param {type: 'number'}  
-cut_icgray_p = "[0.2]*400+[0]*600"#@param {type: 'string'}
-
-"""### Prompts
-`animation_mode: None` will only use the first set. `animation_mode: 2D / Video` will run through them per the set frames and hold on the last one.
-"""
-
-text_prompts = {
-    0: ["Battleship rises out of the sea by Ivan Aivazovsky:10", "yellow color scheme:1"],
-    #100: ["This set of prompts start at frame 100","This prompt has weight five:5"],
-}
-
-image_prompts = {
-    # 0:['ImagePromptsWorkButArentVeryGood.png:2',],
-}
