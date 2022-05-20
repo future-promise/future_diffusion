997b7875740a9fc9ebd1cc2a489c89fc2e66c4a2
Jack English <jackjenglish@gmail.com>
2022-05-20 01:03:23 +0100

    fix print

--- disco.py
+++ disco.py
@@ -34,9 +34,9 @@ def do_run(args):
   for frame_num in range(args.start_frame, args.max_frames):
       if stop_on_next_loop:
         break
-      print("frame num", frame_num)
       
       display.clear_output(wait=True)
+      print("frame num", frame_num)
 
 
       # Inits if not video frames
