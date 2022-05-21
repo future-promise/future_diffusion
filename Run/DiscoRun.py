cdc390bc086090354d897989f3ec51b0a4fd0a65
Jack English <jackjenglish@gmail.com>
2022-05-21 14:37:24 +0100

    core loop

--- Run/DiscoRun.py
+++ Run/DiscoRun.py
@@ -73,23 +73,6 @@ def getInitImage(args):
 
   return init
 
-def applyModel(args, cur_t, x, n):
-  if args.use_secondary_model is True:
-    alpha = torch.tensor(args.diffusion.sqrt_alphas_cumprod[cur_t], device=device, dtype=torch.float32)
-    sigma = torch.tensor(args.diffusion.sqrt_one_minus_alphas_cumprod[cur_t], device=device, dtype=torch.float32)
-    cosine_t = alpha_sigma_to_t(alpha, sigma)
-    out = secondary_model(x, cosine_t[None].repeat([n])).pred
-    fac = args.diffusion.sqrt_one_minus_alphas_cumprod[cur_t]
-    x_in = out * fac + x * (1 - fac)
-    x_in_grad = torch.zeros_like(x_in)
-  else:
-    my_t = torch.ones([n], device=device, dtype=torch.long) * cur_t
-    out = args.diffusion.p_mean_variance(args.model, x, my_t, clip_denoised=False, model_kwargs={'y': y})
-    fac = args.diffusion.sqrt_one_minus_alphas_cumprod[cur_t]
-    x_in = out['pred_xstart'] * fac + x * (1 - fac)
-    x_in_grad = torch.zeros_like(x_in)
-  return out, x_in, x_in_grad
-
 def createModelStats(args, frame_num = 0):
   target_embeds, weights = [], []
 
@@ -125,39 +108,6 @@ def createModelStats(args, frame_num = 0):
   return model_stats
 
 
-def runModelStat(model_stat, t, args, n, x_in, x_in_grad, loss_values):
-  for i in range(args.cutn_batches):
-    t_int = int(t.item())+1 #errors on last step without +1, need to find source
-    #when using SLIP Base model the dimensions need to be hard coded to avoid AttributeError: 'VisionTransformer' object has no attribute 'input_resolution'
-    try:
-        input_resolution=model_stat["clip_model"].visual.input_resolution
-    except:
-        input_resolution=224
-
-    cuts = MakeCutoutsDango(input_resolution,args,
-            Overview= args.cut_overview[1000-t_int], 
-            InnerCrop = args.cut_innercut[1000-t_int], IC_Size_Pow=args.cut_ic_pow, IC_Grey_P = args.cut_icgray_p[1000-t_int]
-            )
-    clip_in = normalize(cuts(x_in.add(1).div(2)))
-    image_embeds = model_stat["clip_model"].encode_image(clip_in).float()
-    dists = spherical_dist_loss(image_embeds.unsqueeze(1), model_stat["target_embeds"].unsqueeze(0))
-    dists = dists.view([args.cut_overview[1000-t_int]+args.cut_innercut[1000-t_int], n, -1])
-    losses = dists.mul(model_stat["weights"]).sum(2).mean(0)
-    loss_values.append(losses.sum().item()) # log loss, probably shouldn't do per cutn_batch
-    x_in_grad += torch.autograd.grad(losses.sum() * args.clip_guidance_scale, x_in)[0] / args.cutn_batches
-
-def sumLosses(args,x_in, out, init):
-  tv_losses = tv_loss(x_in)
-  if args.use_secondary_model is True:
-    range_losses = range_loss(out)
-  else:
-    range_losses = range_loss(out['pred_xstart'])
-  sat_losses = torch.abs(x_in - x_in.clamp(min=-1,max=1)).mean()
-  loss = tv_losses.sum() * args.tv_scale + range_losses.sum() * args.range_scale + sat_losses.sum() * args.sat_scale
-  if init is not None and args.init_scale:
-      init_losses = lpips_model(x_in, init)
-      loss = loss + init_losses.sum() * args.init_scale
-  return loss
 
 
 # removed list:
@@ -175,26 +125,66 @@ def do_run(args):
   # condition_fn = buildConditionFunction(args, cur_t, model_stats, init, loss_values)
   # print('condition fn ', condition_fn)
   cur_t = None
-
+    
   def buildConditionFunction():
     def cond_fn(x, t, y=None):
         with torch.enable_grad():
+            if (cur_t):
+              print('cond_fn: cur_t', cur_t)
+            else:
+              print('cond_fn: no cur_t', cur_t)
             x_is_NaN = False
             x = x.detach().requires_grad_()
             n = x.shape[0]
-
-            out, x_in, x_in_grad = applyModel(args, cur_t, x, n)
-
-            # only has length 1
+            if args.use_secondary_model is True:
+              alpha = torch.tensor(args.diffusion.sqrt_alphas_cumprod[cur_t], device=device, dtype=torch.float32)
+              sigma = torch.tensor(args.diffusion.sqrt_one_minus_alphas_cumprod[cur_t], device=device, dtype=torch.float32)
+              cosine_t = alpha_sigma_to_t(alpha, sigma)
+              out = secondary_model(x, cosine_t[None].repeat([n])).pred
+              fac = args.diffusion.sqrt_one_minus_alphas_cumprod[cur_t]
+              x_in = out * fac + x * (1 - fac)
+              x_in_grad = torch.zeros_like(x_in)
+            else:
+              my_t = torch.ones([n], device=device, dtype=torch.long) * cur_t
+              out = args.diffusion.p_mean_variance(args.model, x, my_t, clip_denoised=False, model_kwargs={'y': y})
+              fac = args.diffusion.sqrt_one_minus_alphas_cumprod[cur_t]
+              x_in = out['pred_xstart'] * fac + x * (1 - fac)
+              x_in_grad = torch.zeros_like(x_in)
             for model_stat in model_stats:
-              runModelStat(model_stat, t, args, n, x_in, x_in_grad, loss_values)
+              for i in range(args.cutn_batches):
+                  t_int = int(t.item())+1 #errors on last step without +1, need to find source
+                  #when using SLIP Base model the dimensions need to be hard coded to avoid AttributeError: 'VisionTransformer' object has no attribute 'input_resolution'
+                  try:
+                      input_resolution=model_stat["clip_model"].visual.input_resolution
+                  except:
+                      input_resolution=224
 
-            loss = sumLosses(args, x_in, out, init)
-
+                  cuts = MakeCutoutsDango(input_resolution,args,
+                          Overview= args.cut_overview[1000-t_int], 
+                          InnerCrop = args.cut_innercut[1000-t_int], IC_Size_Pow=args.cut_ic_pow, IC_Grey_P = args.cut_icgray_p[1000-t_int]
+                          )
+                  clip_in = normalize(cuts(x_in.add(1).div(2)))
+                  image_embeds = model_stat["clip_model"].encode_image(clip_in).float()
+                  dists = spherical_dist_loss(image_embeds.unsqueeze(1), model_stat["target_embeds"].unsqueeze(0))
+                  dists = dists.view([args.cut_overview[1000-t_int]+args.cut_innercut[1000-t_int], n, -1])
+                  losses = dists.mul(model_stat["weights"]).sum(2).mean(0)
+                  loss_values.append(losses.sum().item()) # log loss, probably shouldn't do per cutn_batch
+                  x_in_grad += torch.autograd.grad(losses.sum() * args.clip_guidance_scale, x_in)[0] / args.cutn_batches
+            tv_losses = tv_loss(x_in)
+            if args.use_secondary_model is True:
+              range_losses = range_loss(out)
+            else:
+              range_losses = range_loss(out['pred_xstart'])
+            sat_losses = torch.abs(x_in - x_in.clamp(min=-1,max=1)).mean()
+            loss = tv_losses.sum() * args.tv_scale + range_losses.sum() * args.range_scale + sat_losses.sum() * args.sat_scale
+            if init is not None and args.init_scale:
+                init_losses = lpips_model(x_in, init)
+                loss = loss + init_losses.sum() * args.init_scale
             x_in_grad += torch.autograd.grad(loss, x_in)[0]
             if torch.isnan(x_in_grad).any()==False:
                 grad = -torch.autograd.grad(x_in, x, x_in_grad)[0]
             else:
+              # print("NaN'd")
               x_is_NaN = True
               grad = torch.zeros_like(x)
         if args.clamp_grad and x_is_NaN == False:
@@ -212,81 +202,82 @@ def do_run(args):
 
 
   image_display = Output()
-  i = 0
-  if args.animation_mode == 'None':
-    # display.clear_output(wait=True)
-    batchBar = tqdm(range(args.n_batches), desc ="Batches")
-    batchBar.n = i
-    batchBar.refresh()
-  display.display(image_display)
-  gc.collect()
-  torch.cuda.empty_cache()
-  cur_t = args.diffusion.num_timesteps - args.skip_steps - 1
-  total_steps = cur_t
+  for i in range(args.n_batches):
+      if args.animation_mode == 'None':
+        # display.clear_output(wait=True)
+        batchBar = tqdm(range(args.n_batches), desc ="Batches")
+        batchBar.n = i
+        batchBar.refresh()
+      print('')
+      display.display(image_display)
+      gc.collect()
+      torch.cuda.empty_cache()
+      cur_t = args.diffusion.num_timesteps - args.skip_steps - 1
+      total_steps = cur_t
 
-  if args.perlin_init:
-      init = regen_perlin()
+      if args.perlin_init:
+          init = regen_perlin()
 
-  if args.diffusion_sampling_mode == 'ddim':
-      samples = sample_fn(
-          args.model,
-          (args.batch_size, 3, args.side_y, args.side_x),
-          clip_denoised=args.clip_denoised,
-          model_kwargs={},
-          cond_fn=condition_fn,
-          progress=True,
-          skip_timesteps=args.skip_steps,
-          init_image=init,
-          randomize_class=args.randomize_class,
-          eta=args.eta,
-      )
-  else:
-      samples = sample_fn(
-          args.model,
-          (args.batch_size, 3, args.side_y, args.side_x),
-          clip_denoised=args.clip_denoised,
-          model_kwargs={},
-          cond_fn=condition_fn,
-          progress=True,
-          skip_timesteps=args.skip_steps,
-          init_image=init,
-          randomize_class=args.randomize_class,
-          order=2,
-      )
-  
-  print('samples', samples)
-  # with run_display:
-  for j, sample in enumerate(samples):    
-    cur_t -= 1
-    intermediateStep = False
-    if args.steps_per_checkpoint is not None:
-        if j % args.steps_per_checkpoint == 0 and j > 0:
+      if args.diffusion_sampling_mode == 'ddim':
+          samples = sample_fn(
+              args.model,
+              (args.batch_size, 3, args.side_y, args.side_x),
+              clip_denoised=args.clip_denoised,
+              model_kwargs={},
+              cond_fn=condition_fn,
+              progress=True,
+              skip_timesteps=args.skip_steps,
+              init_image=init,
+              randomize_class=args.randomize_class,
+              eta=args.eta,
+          )
+      else:
+          samples = sample_fn(
+              args.model,
+              (args.batch_size, 3, args.side_y, args.side_x),
+              clip_denoised=args.clip_denoised,
+              model_kwargs={},
+              cond_fn=condition_fn,
+              progress=True,
+              skip_timesteps=args.skip_steps,
+              init_image=init,
+              randomize_class=args.randomize_class,
+              order=2,
+          )
+      
+      print('samples', samples)
+      # with run_display:
+      for j, sample in enumerate(samples):    
+        cur_t -= 1
+        intermediateStep = False
+        if args.steps_per_checkpoint is not None:
+            if j % args.steps_per_checkpoint == 0 and j > 0:
+              intermediateStep = True
+        elif j in args.intermediate_saves:
           intermediateStep = True
-    elif j in args.intermediate_saves:
-      intermediateStep = True
-    with image_display:
-      if j % args.display_rate == 0 or cur_t == -1 or intermediateStep == True:
-          for k, image in enumerate(sample['pred_xstart']):
-              # tqdm.write(f'Batch {i}, step {j}, output {k}:')
-              current_time = datetime.now().strftime('%y%m%d-%H%M%S_%f')
-              percent = math.ceil(j/total_steps*100)
-              if args.n_batches > 0:
-                #if intermediates are saved to the subfolder, don't append a step or percentage to the name
-                if cur_t == -1 and args.intermediates_in_subfolder is True:
-                  save_num = f'{frame_num:04}' if args.animation_mode != "None" else i
-                  filename = f'{args.batch_name}({args.batchNum})_{save_num}.png'
-                else:
-                  #If we're working with percentages, append it
-                  if args.steps_per_checkpoint is not None:
-                    filename = f'{args.batch_name}({args.batchNum})_{i:04}-{percent:02}%.png'
-                  # Or else, iIf we're working with specific steps, append those
-                  else:
-                    filename = f'{args.batch_name}({args.batchNum})_{i:04}-{j:03}.png'
-              image = TF.to_pil_image(image.add(1).div(2).clamp(0, 1))
-              if j % args.display_rate == 0 or cur_t == -1:
-                image.save('progress.png')
-                # display.clear_output(wait=True)
-                display.display(display.Image('progress.png'))
-              saveImage(args, image, cur_t, j, filename)
-  
-  plt.plot(np.array(loss_values), 'r')
+        with image_display:
+          if j % args.display_rate == 0 or cur_t == -1 or intermediateStep == True:
+              for k, image in enumerate(sample['pred_xstart']):
+                  # tqdm.write(f'Batch {i}, step {j}, output {k}:')
+                  current_time = datetime.now().strftime('%y%m%d-%H%M%S_%f')
+                  percent = math.ceil(j/total_steps*100)
+                  if args.n_batches > 0:
+                    #if intermediates are saved to the subfolder, don't append a step or percentage to the name
+                    if cur_t == -1 and args.intermediates_in_subfolder is True:
+                      save_num = f'{frame_num:04}' if args.animation_mode != "None" else i
+                      filename = f'{args.batch_name}({args.batchNum})_{save_num}.png'
+                    else:
+                      #If we're working with percentages, append it
+                      if args.steps_per_checkpoint is not None:
+                        filename = f'{args.batch_name}({args.batchNum})_{i:04}-{percent:02}%.png'
+                      # Or else, iIf we're working with specific steps, append those
+                      else:
+                        filename = f'{args.batch_name}({args.batchNum})_{i:04}-{j:03}.png'
+                  image = TF.to_pil_image(image.add(1).div(2).clamp(0, 1))
+                  if j % args.display_rate == 0 or cur_t == -1:
+                    image.save('progress.png')
+                    # display.clear_output(wait=True)
+                    display.display(display.Image('progress.png'))
+                  saveImage(args, image, cur_t, j, filename)
+      
+      plt.plot(np.array(loss_values), 'r')
